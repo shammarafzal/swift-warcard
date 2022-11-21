1a46d12e691b36b023c985ca57770d155b39930a
Ammar Afzal <ammar.afzal@meshlane.cloud>
2022-11-21 12:08:41 +0500

    Initial Commit

---
+++ WarCard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WarCard
+//
+//  Created by Ammar Afzal on 21/11/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WarCard/WarCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  WarCardApp.swift
+//  WarCard
+//
+//  Created by Ammar Afzal on 21/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct WarCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
