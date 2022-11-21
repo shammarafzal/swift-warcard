6a0499af65952b787e61b5bcd1384b14abae2107
Ammar Afzal <ammar.afzal@meshlane.cloud>
2022-11-21 15:28:20 +0500

    warcard project setup

--- WarCard/ContentView.swift
+++ WarCard/ContentView.swift
@@ -9,13 +9,38 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack{
+            ZStack{
+                Image("toronto")
+                    .cornerRadius(/*@START_MENU_TOKEN@*/10.0/*@END_MENU_TOKEN@*/)
+                VStack{
+                    Text("CN Tower").font(.title).foregroundColor(Color.white)
+                    Text("Toronto").font(.subheadline).foregroundColor(Color.white)
+                    
+                }
+              
+                .padding(/*@START_MENU_TOKEN@*/.all, 20.0/*@END_MENU_TOKEN@*/).background(Color.black.opacity(0.7))
+            }
+            
+            
+            ZStack{
+                Image("london")
+                    .cornerRadius(/*@START_MENU_TOKEN@*/10.0/*@END_MENU_TOKEN@*/)
+                VStack{
+                    Text("Big Ben").font(.title).foregroundColor(Color.white)
+                    Text("London").font(.subheadline).foregroundColor(Color.white)
+                    
+                }
+              
+                .padding(/*@START_MENU_TOKEN@*/.all, 20.0/*@END_MENU_TOKEN@*/).background(Color.black.opacity(0.7))
+            }
     }
+}
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewDevice("iPhone 13 Pro Max")
     }
 }
